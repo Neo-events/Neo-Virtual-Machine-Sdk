abb1dcc191e0b1801f770bdf866cfa3a30000754
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-09-07 17:27:38 -0400

    Initial Project Commit

---
+++ src/NeoEvents.VirtualMachine/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Licensed to the "Neo Events" under one or more agreements.
+// The "Neo Events" licenses this file to you under the GPL-3.0 license.
+
+namespace NeoEvents.VirtualMachine;
+
+public class Class1
+{
+
+}
