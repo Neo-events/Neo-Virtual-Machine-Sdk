fc88eca551d39397cf1cc4dee391497ecd5f6b12
Christopher Schuchardt <cschuchardt88@gmail.com>
2024-09-08 15:05:44 -0400

    Added more methods to `ScriptBuilder`

--- src/NeoEvents.VirtualMachine/ScriptBuilder.cs
+++ src/NeoEvents.VirtualMachine/ScriptBuilder.cs
@@ -2,7 +2,9 @@
 // The "Neo Events" licenses this file to you under the GPL-3.0 license.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Numerics;
 
 namespace NeoEvents.VirtualMachine;
 
@@ -13,20 +15,23 @@ public class ScriptBuilder : IDisposable
 
     public int Length => checked((int)_ms.Position);
 
-    public ScriptBuilder()
+    private ScriptBuilder()
     {
         _writer = new BinaryWriter(_ms);
     }
 
+    public static ScriptBuilder Empty() => new();
+
     public void Dispose()
     {
-        GC.SuppressFinalize(this);
         _writer.Dispose();
         _ms.Dispose();
+        GC.SuppressFinalize(this);
     }
 
     public byte[] Build()
     {
+        _writer.Flush();
         return _ms.ToArray();
     }
 
@@ -40,17 +45,20 @@ public class ScriptBuilder : IDisposable
         return this;
     }
 
-    public ScriptBuilder EmitCall(int offset)
+    public ScriptBuilder Call(int offset)
     {
         if (offset < sbyte.MinValue || offset > sbyte.MaxValue)
             return Emit(OpCode.CALL_L, BitConverter.GetBytes(offset));
         return Emit(OpCode.CALL, [(byte)offset]);
     }
 
-    public ScriptBuilder EmitPush(bool value) =>
+    public ScriptBuilder EmitSysCall(uint api) =>
+        Emit(OpCode.SYSCALL, BitConverter.GetBytes(api));
+
+    public ScriptBuilder Push(bool value) =>
         Emit(value ? OpCode.PUSHT : OpCode.PUSHF);
 
-    public ScriptBuilder EmitPush(byte[] data)
+    public ScriptBuilder Push(byte[] data)
     {
         if (data.Length == 0)
             return Emit(OpCode.PUSHDATA1, [0, 0]);
@@ -62,12 +70,86 @@ public class ScriptBuilder : IDisposable
             return Emit(OpCode.PUSHDATA4, [.. BitConverter.GetBytes(data.Length), .. data]);
     }
 
-    public ScriptBuilder EmitPush(string data)
+    public ScriptBuilder Push(string value)
     {
         StrictUTF8 strictUTF8 = new();
-        return EmitPush(strictUTF8.GetBytes(data));
+        return Push(strictUTF8.GetBytes(value));
     }
 
-    public ScriptBuilder EmitSysCall(uint api) =>
-        Emit(OpCode.SYSCALL, BitConverter.GetBytes(api));
+    public ScriptBuilder Push(BigInteger value)
+    {
+        if (value >= -1 && value <= 16)
+            return Emit(OpCode.PUSH0 + (byte)value);
+
+        Span<byte> buffer = stackalloc byte[32];
+
+        if (value.TryWriteBytes(buffer, out var bytesWritten, isUnsigned: false, isBigEndian: false) == false)
+            throw new ArgumentOutOfRangeException(nameof(value));
+
+        return bytesWritten switch
+        {
+            1 => Emit(OpCode.PUSHINT8, PadRight(buffer, bytesWritten, 1, value.Sign < 0)),
+            2 => Emit(OpCode.PUSHINT16, PadRight(buffer, bytesWritten, 2, value.Sign < 0)),
+            <= 4 => Emit(OpCode.PUSHINT32, PadRight(buffer, bytesWritten, 4, value.Sign < 0)),
+            <= 8 => Emit(OpCode.PUSHINT64, PadRight(buffer, bytesWritten, 8, value.Sign < 0)),
+            <= 16 => Emit(OpCode.PUSHINT128, PadRight(buffer, bytesWritten, 16, value.Sign < 0)),
+            _ => Emit(OpCode.PUSHINT256, PadRight(buffer, bytesWritten, 32, value.Sign < 0)),
+        };
+
+        static byte[] PadRight(Span<byte> buffer, int dataLength, int padLength, bool isNegative)
+        {
+            var padByte = isNegative ? (byte)0xff : (byte)0x00;
+            for (var i = dataLength; i < padLength; i++)
+                buffer[i] = padByte;
+            return buffer[..padLength].ToArray();
+        }
+    }
+
+    public ScriptBuilder Push(object? value) =>
+        value switch
+        {
+            null => PushNull(),
+            byte[] data => Push(data),
+            string str => Push(str),
+            bool b => Push(b),
+            BigInteger bi => Push(bi),
+            sbyte s => Push(s),
+            byte b => Push(b),
+            short s => Push(s),
+            ushort us => Push(us),
+            int i => Push(i),
+            uint ui => Push(ui),
+            long l => Push(l),
+            ulong ul => Push(ul),
+            char c => Push((ushort)c),
+            Enum e => Push(BigInteger.Parse(e.ToString("d"))),
+            _ => throw new ArgumentException($"{value.GetType().FullName} can't be Serialized.", nameof(value)),
+        };
+
+    public ScriptBuilder PushNull() =>
+        Emit(OpCode.PUSHNULL);
+
+    public ScriptBuilder CreateArray<T>(IReadOnlyList<T> array)
+        where T : notnull
+    {
+        if (array.Count == 0)
+            return Emit(OpCode.NEWARRAY0);
+        for (var i = array.Count - 1; i >= 0; i--)
+            Push(array[i]);
+        Push(array.Count);
+        return Emit(OpCode.PACK);
+    }
+
+    public ScriptBuilder CreateMap<TKey, TValue>(IReadOnlyDictionary<TKey, TValue> map)
+    {
+        Emit(OpCode.NEWMAP);
+        foreach (var (key, value) in map)
+        {
+            Emit(OpCode.DUP);
+            Push(key);
+            Push(value);
+            Emit(OpCode.SETITEM);
+        }
+        return this;
+    }
 }
